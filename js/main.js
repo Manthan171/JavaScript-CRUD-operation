aa04c63f501ea6926a54247c2e927aa08da805a5
Manthan171 <mmparmar17122000@gmail.com>
2022-04-05 12:17:26 +0530

    add edit functionality

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
 let isEdit = false;
-displayData();
+let editIndex = -1;
+window.onload = displayData();
 // < --------------- Get Data ------------------>
 function getData() {
-  let bookId = makeid(5);
+  let data = JSON.parse(localStorage.getItem("data")) || [];
+  let bookId = isEdit ? data[editIndex].bookId : makeid(4);
   let bookName = document.getElementById("book-name").value;
   let authorName = document.getElementById("author-name").value;
   let category = document.getElementById("category").value;
   let price = document.getElementById("price").value;
 
-  let data = JSON.parse(localStorage.getItem("data")) || [];
   console.log(data);
+  const updatedData = {
+    bookId,
+    bookName,
+    authorName,
+    category,
+    price,
+  };
+  if(isEdit){
+    isEdit = false;
+    data[editIndex] = updatedData;
+    editIndex = -1;
+  }else{
+    data.push(updatedData);
+  }
 
-  data.push({
-    bookId: bookId,
-    bookName: bookName,
-    authorName: authorName,
-    category: category,
-    price: price,
-  });
 
   localStorage.setItem("data", JSON.stringify(data));
   displayData();
@@ -26,7 +34,7 @@ function getData() {
 // < --------------------- Generate random string for book id ---------------->
 function makeid(length) {
   var result = "";
-  var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  var characters = "0123456789";
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -38,11 +46,11 @@ function makeid(length) {
 function displayData() {
   let createData = "";
   let tableData = document.getElementById("table-info");
-  console.log("dfsdfdsg", tableData);
+  // console.log("dfsdfdsg", tableData);
   let getData = JSON.parse(localStorage.getItem("data"));
   console.log("getdata", getData);
   getData.forEach((element, index) => {
-    console.log(index);
+    // console.log(index);
     createData += `<tr>
                         <td>${element.bookId}</td>
                         <td>${element.bookName}</td>
@@ -50,7 +58,7 @@ function displayData() {
                         <td>${element.category}</td>
                         <td>${element.price}</td>
                         <td class="btn">
-                            <button id="editBtn"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+                            <button id="editBtn" onclick = "editbtn(${index})"><i class="fa fa-pencil" aria-hidden="true"></i></button>
                             <button onclick = "deleteBtn(${index})"><i class="fa fa-trash" aria-hidden="true"></i></button>
                         </td>
                     </tr>`;
@@ -60,13 +68,24 @@ function displayData() {
 
 // < ------------------------ Edit functionality ------------------------------>
 
-// function editbtn(){
-//     // isEdit = true;
-//     // editIndex = i;
-//     document.getElementById("main-btn").innerHTML = "Save Data";
-//     const editData = JSON.parse(localStorage.getItem('data'))||[];
-//     console.log(editData);
-// }
+function editbtn(index){
+    isEdit = true;
+    editIndex = index;
+    document.getElementById("main-btn").innerHTML = "Save Data";
+    const editData = JSON.parse(localStorage.getItem('data'))||[];
+    console.log(editData);
+
+    let bookName = editData[index].bookName;
+    let author = editData[index].authorName;
+    let bookCat = editData[index].category;
+    let bookPrice = editData[index].price;
+
+    document.getElementById("book-name").value = bookName;
+    document.getElementById("author-name").value = author;
+    document.getElementById("category").value = bookCat;
+    document.getElementById("price").value = bookPrice;
+    console.log(bookName);
+}
 
 // < -------------------- Delete Functionality ---------------------->
 
